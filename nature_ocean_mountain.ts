d68a63ac516e56c1ea5232c59a689c05a0c6b804
bkesyakami <136637622+bkesyakami@users.noreply.github.com>
2023-06-25 13:42:31 +0700

    Add code comments

---
+++ nature_ocean_mountain.ts
@@ -0,0 +1,49 @@
+const powerOfPlay: string[] = 
+[
+ "Play can foster creativity, language acquisition and imagination.",
+ "It can help children develop healthy self-esteem and confidence.",
+ "Children can learn to take turns and share through play.",
+ "Playing together can help to build friendships and strong relationships.",
+ "Playing can help to develop children’s motor skills, concentration and resilience.",
+ "It can help to ease transitions and encourage positive behavior.",
+ "Play can help teach children about follow rules and respect others.",
+ "It can help children build important problem-solving skills.",
+ "Play can stimulate cognitive, emotional, and physical development.",
+ "It can help to enhance children’s social skills and their understanding of different perspectives.",
+ "Play is a great way for children to explore their emotions and feelings.",
+ "Playing can help children experience success in different tasks and activities.",
+ "It can be an effective way to help children to make sense of the world.",
+ "Playing can help children to become more independent and self-confident.",
+ "It can help children to be creative in different ways and express themselves.",
+ "Play can be an important part of learning to regulate emotions and behaviour.",
+ "It can help children to feel safe, secure and supported.",
+ "Playing together can help to build trust and cooperation.",
+ "It can help to reduce aggression and increase empathy.",
+ "Engaging in play can help children to develop better problem-solving and communication skills.",
+ "Playing can help to stimulate the brain and help with cognitive development.",
+ "It can be an effective way to help children learn to manage their emotions.",
+ "Play is an important part of healthy child development.",
+ "It can help children learn to cope with stress and develop resilience.",
+ "Playing can help to foster imagination and creativity in children.",
+ "It can help to improve coordination and gross motor skills.",
+ "Engaging in play can help children to understand and express their emotions.",
+ "It can help to develop social skills and increase children’s self-esteem.",
+ "Play can help to develop children’s problem-solving and decision-making skills.",
+ "It can help to teach children how to cooperate and to work together as a team.",
+ "Playing can help to build trust and friendship between children and adults.",
+ "It can be a great way for children to explore and develop their sense of curiosity.",
+ "Engaging in play can help to strengthen the bond between children and their families.",
+ "It can help to reduce stress and help children to learn and reach their potential.",
+ "Playing can be a great way to help children learn about the world around them.",
+ "It can help to foster an appreciation for nature and the environment.",
+ "Engaging in play can help to provide children with lifelong skills.",
+ "It can help to encourage children to have a positive outlook and to think creatively.",
+];
+
+function logPowerOfPlay(): void {
+    for (const message of powerOfPlay) {
+        console.log(message);
+    }
+}
+
+logPowerOfPlay();
