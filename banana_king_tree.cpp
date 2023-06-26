e76e39a16b0e07da311a6a8f917607b9b91793be
bkesyakami <136637622+bkesyakami@users.noreply.github.com>
2023-06-26 21:26:33 +0700

    Fix rendering performance

---
+++ banana_king_tree.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+int main()
+{
+	cout << "The Power of Play" << endl << endl;
+
+	string message1 = "Play is an essential part of our lives. It helps us explore, connect, grow, and learn more about ourselves and the world around us.";
+	cout << message1 << endl << endl;
+
+	string message2 = "We start playing almost as soon as we’re born, and it continues to be a powerful part of our lives throughout our entire lives. Play helps us to develop skills, build relationships, cope with difficult emotions, and let go of stress and worry.";
+	cout << message2 << endl << endl;
+
+	string message3 = "No matter our age, play can bring us joy and can help us find a sense of balance and purpose. It can also be an opportunity for creativity and problem solving.";
+	cout << message3 << endl << endl;
+
+	string message4 = "Whether your idea of play is picking up a new hobby, heading outdoors, taking up a sport, or playing a board game, the power of play can offer countless rewards. Taking time to play can bring a sense of freedom and adventure, and offer us a chance to come together with friends and family and share in experiences.";
+	cout << message4 << endl << endl;
+
+	string message5 = "Playing is not only fun, but it can also reduce stress, help us build important life skills, and even bring us closer to our friends and family. So no matter what your age, give yourself the gift of play and explore the power it can bring into your life!";
+	cout << message5 << endl << endl;
+
+	string message6 = "So go ahead and give yourself permission to play. Who knows, it could be the start of something amazing!";
+	cout << message6 << endl;
+
+	return 0;
+}
