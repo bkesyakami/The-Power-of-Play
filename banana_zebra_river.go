e466c137d5c36fc70364f63cc82828600b880f28
bkesyakami <136637622+bkesyakami@users.noreply.github.com>
2023-07-20 14:57:17 +0700

    Refactor business logic

---
+++ banana_zebra_river.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+// The declared function "powerOfPlay" will represent
+// the main concept of the topic. 
+func powerOfPlay() {
+	
+	// Introduction 
+	fmt.Println("The Power of Play")
+	fmt.Println("From infancy, playing is an essential part of human life. It has a long-lasting impact on how our brains develop, how we interact with each other, and how we make decisions. The benefits of play are both psychological and physical, and the effects extend beyond childhood.")
+	
+	// Playing Teaches Self-Regulation
+	fmt.Println("\nPlaying Teaches Self-Regulation")
+	fmt.Println("Play helps us to learn how to regulate emotions and behavior. Through play, children develop the skills they need to practice self-regulation – the ability to manage their own emotions and behavior and, ultimately, the ability to make responsible decisions.")
+	
+	// Playing Encourages Social Interaction
+	fmt.Println("\nPlaying Encourages Social Interaction")
+	fmt.Println("Play helps children to learn to interact with each other – how to share, take turns, negotiate, and resolve conflicts. It also helps them to develop the communication and problem-solving skills necessary for social interaction.")
+	
+	// Playing Improves Cognitive Skills
+	fmt.Println("\nPlaying Improves Cognitive Skills")
+	fmt.Println("Play is essential for the development of cognitive skills. Through play, children learn to experiment, explore, and discover, which helps them to develop critical thinking and problem-solving skills. Play also helps to develop language and memory skills.")
+	
+	// Playing Supports Emotional Development
+	fmt.Println("\nPlaying Supports Emotional Development")
+	fmt.Println("Play helps children to develop self-confidence and self-esteem. Through play, children learn to express their emotions and work out their feelings. Play also provides an opportunity for children to practice their emotional regulation skills.")
+	
+	// Playing Promotes Physical Development
+	fmt.Println("\nPlaying Promotes Physical Development")
+	fmt.Println("Play helps to develop motor skills, coordination, balance, and physical strength. Through play, children learn how their bodies move and how they interact with their physical environment. Play also helps to develop physical endurance and stamina.")
+	
+	// Playing Improves Resilience
+	fmt.Println("\nPlaying Improves Resilience")
+	fmt.Println("Play helps children to develop the skills necessary to handle stressors and cope with difficult situations. Through play, children learn to take risks, manage their emotions, and develop problem-solving strategies. Play also helps to build resilience – the ability to bounce back from setbacks and challenges.")
+	
+	// Playing Fosters Imagination
+	fmt.Println("\nPlaying Fosters Imagination")
+	fmt.Println("Through play, children learn to use their imagination and to develop creative thinking skills. Play helps them to explore new ideas, to express themselves, and to develop their own unique perspective on the world.")
+	
+	// Conclusion 
+	fmt.Println("\nConclusion")
+	fmt.Println("The power of play is undeniable. From infancy, play helps to develop the skills we need for success in life. It encourages physical, cognitive, emotional, and social development. And it fosters creativity and imagination. Playing is essential for the healthy development of children – and for the healthy functioning of society.")
+}
+
+func main() {
+	powerOfPlay()
+}
